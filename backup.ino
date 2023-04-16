e790039ab2e52debd96177cba5f3fb5f914f62eb
JinTai6 <96611286+JinTai6@users.noreply.github.com>
2023-04-16 22:14:57 +0800

    Update backup.ino

--- backup.ino
+++ backup.ino
@@ -1,4 +1,4 @@
-#define BLINKER_WIFI
+define BLINKER_WIFI
 #include <Blinker.h>
 #include <DHT.h>
 #include <Wire.h>
@@ -7,10 +7,14 @@
 #define DHTPIN 15 // 温湿度传感器引脚
 #define LDRPIN 36 // 光敏电阻引脚
 #define SOILPIN1 32 // 土壤湿度传感器1引脚
+#define SOILPIN2 33 // 土壤湿度传感器2引脚
 #define DHTPIN_SOIL 4 // 土壤温湿度传感器引脚
 #define FLAMEPIN 16 // 火焰传感器引脚
+#define SMOKEPIN 34 //烟雾传感器引脚
 #define BUZZERPIN 26 // 蜂鸣器引脚
 #define LEDPIN 27 // LED引脚
+#define LEDPIN1 11 //1号LED补光灯针脚
+#define LEDPIN2 10 //2号LED补光灯针脚
 
 LiquidCrystal_I2C lcd(32,16,2);  // 设置I2C地址和屏幕行列数
 
@@ -25,6 +29,7 @@ BlinkerNumber HUMI("humi"); // 湿度数据流
 BlinkerNumber TEMP("temp"); // 温度数据流
 BlinkerNumber LIGHT("light"); // 光照数据流
 BlinkerNumber SOIL1("soil1"); // 土壤湿度数据流1
+BlinkerNumber SOIL2("soil2"); // 土壤湿度数据流2
 BlinkerNumber SOIL_TEMP("soil_temp");// 土壤温度数据流
 BlinkerNumber SOIL_HUMI("soil_humi");// 土壤湿度数据流2
 
@@ -35,7 +40,6 @@ float humi_read = 0, temp_read = 0, light_read = 0, soil_read1 = 0, soil_read2 =
 float soil_temp_read = 0, soil_humi_read = 0;
 unsigned long previousMillis = 0;
 const unsigned long interval = 2000;
-const int SMOKE_THRESHOLD = 2000;
 
 void heartbeat()
 {
@@ -43,6 +47,7 @@ HUMI.print(humi_read); // 发送湿度数据流
 TEMP.print(temp_read); // 发送温度数据流
 LIGHT.print(light_read); // 发送光照数据流
 SOIL1.print(soil_read1); // 发送土壤湿度数据流1
+SOIL2.print(soil_read2); // 发送土壤湿度数据流2
 SOIL_TEMP.print(soil_temp_read);// 发送土壤温度数据流
 SOIL_HUMI.print(soil_humi_read);// 发送土壤湿度数据流2
 }
@@ -53,6 +58,7 @@ Blinker.dataStorage("temp", temp_read); // 存储温度数据
 Blinker.dataStorage("humi", humi_read); // 存储湿度数据
 Blinker.dataStorage("light", light_read); // 存储光照数据
 Blinker.dataStorage("soil1", soil_read1); // 存储土壤湿度数据1
+Blinker.dataStorage("soil2", soil_read2); // 存储土壤湿度数据2
 Blinker.dataStorage("soil_temp", soil_temp_read);// 存储土壤温度数据
 Blinker.dataStorage("soil_humi", soil_humi_read);// 存储土壤湿度数据2
 }
@@ -77,8 +83,33 @@ dht_soil.begin();// 初始化土壤湿度传感器
 lcd.begin(); // 初始化液晶屏
 }
 
+bool checkFlame() {
+  int flameValue = digitalRead(FLAMEPIN);
+  if (flameValue == LOW) {  // 火焰检测到，返回真
+    return true;
+  } else {  // 火焰未检测到，返回假
+    return false;
+  }
+}
+
+void alert(bool flameDetected) {
+  if (flameDetected) {  // 火焰检测到
+    digitalWrite(BUZZERPIN, HIGH);  // 打开蜂鸣器
+    digitalWrite(LEDPIN, HIGH);     // 打开LED灯
+  } else {  // 火焰未检测到
+    digitalWrite(BUZZERPIN, LOW);   // 关闭蜂鸣器
+    digitalWrite(LEDPIN, LOW);      // 关闭LED灯
+  }
+}
+
 void loop()
 {
+
+//检测是否存在火焰
+bool flameDetected = checkFlame();  // 检测火焰状态
+  alert(flameDetected);  // 控制蜂鸣器和LED灯报警
+  delay(100);  // 等待一段时间后继续进行检测
+
 Blinker.run();// 运行Blinker库
 // 读取DHT传感器、LDR传感器和土壤湿度传感器的数据
 unsigned long currentMillis = millis();
@@ -89,6 +120,7 @@ float h = dht.readHumidity();
 float t = dht.readTemperature();
 int l = analogRead(LDRPIN);
 int s1 = analogRead(SOILPIN1);
+int s2 = analogRead(SOILPIN2);
 float h_soil = dht_soil.readHumidity();
 float t_soil = dht_soil.readTemperature();
 
@@ -102,7 +134,7 @@ float t_soil = dht_soil.readTemperature();
   lcd.print(h);                    // 打印湿度值
   lcd.print("%");                  // 打印湿度单位 "%"
   
-  delay(5000);                     // 延迟5秒
+  delay(1000);                     // 延迟5秒
 // 如果无法读取DHT传感器的数据
 if (isnan(h) || isnan(t)) {
 BLINKER_LOG("Failed to read from DHT sensor!");
@@ -143,16 +175,20 @@ soil_temp_read = t_soil;
 light_read = map(l, 0, 4095, 100, 0);
 // 将土壤湿度传感器1的数据映射到0-100的范围内
 soil_read1 = map(s1, 4095, 0, 0, 100);
+// 将土壤湿度传感器2的数据映射到0-100的范围内
+soil_read2 = map(s2, 4095, 0, 0, 100);
 
 // 打印光照和土壤湿度1的数据到串口
 BLINKER_LOG("Light: ", light_read, " %");
 BLINKER_LOG("Soil Moisture 1: ", soil_read1, " %");
+BLINKER_LOG("Soil Moisture 2: ", soil_read2, " %");
 
 // 将湿度、温度、光照和土壤湿度1的数据发送到点灯APP中
 HUMI.print(humi_read);
 TEMP.print(temp_read);
 LIGHT.print(light_read);
 SOIL1.print(soil_read1);
+SOIL2.print(soil_read2);
 SOIL_TEMP.print(soil_temp_read);
 SOIL_HUMI.print(soil_humi_read);
 }
